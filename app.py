2b3898f04d0cecb4414fd7cecc9d01ed157ec4a0
Christopher <chrisfree1122@gmail.com>
2023-10-12 13:29:31 -0500

    creando validaciones para camparar con el excel institucional

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
 # Variables para almacenar los nombres de estudiantes de ambos archivos
-
 nombres_profesora = []
 nombres_institucional = []
+
+# Columnas de calificaciones en el Excel de la profesora
+columnas_calificaciones_profesora = ["Primer Parcial", "Segundo Parcial", "Nota Final"]
 
 def normalizar_nombre(nombre):
     # Esta función normaliza el nombre eliminando espacios en blanco adicionales
     # y convirtiéndolo a minúsculas
     return nombre.strip().lower()
 
-# sistema de interfas del sistema de como tener en claro para todo el sintema de inferzad  de omomc de tener de 
 def cargar_archivo_profesora():
     archivo = filedialog.askopenfilename(filetypes=[("Archivos de Excel", "*.xlsx")])
     if archivo:
@@ -25,27 +26,62 @@ def cargar_archivo_profesora():
         # Habilitar el botón "Subir Excel Institucional"
         subir_institucional_button["state"] = "normal"
 
-
 def cargar_archivo_institucional():
-    mensaje_advertencia = "En el Excel que subió anteriormente, los nombres de sus estudiantes, deben estar en el Excel institucional para poder transferir las calificaciones. \nCaso contrario no se preocederá a transferir."
-    mensaje_adicional = ""
-    mensaje_recordatorio = "¡Recuerde! Los nombres de sus estudiantes, deben ser los mismos que estan en el Excel Institucional"
+    global nombres_institucional  # Mueve esta línea al principio de la función
+
+    if not nombres_profesora:
+        messagebox.showinfo("Advertencia", "Primero suba el archivo Excel de la profesora.", icon='warning', default='ok')
+        return
+
+    if nombres_institucional:
+        mensaje_adicional = "Los nombres de sus estudiantes deben coincidir con el Excel Institucional.\nRecuerde que solo puede subir el Excel Institucional una vez."
+        ventana_no_encontrados(mensaje_adicional, [])
+        return
+    
+    mensaje_advertencia = "En el Excel que subió anteriormente, los nombres de sus estudiantes deben estar en el Excel institucional para poder transferir las calificaciones. \nCaso contrario no se procederá a transferir."
     
     messagebox.showinfo("Dato Importante", mensaje_advertencia, icon='info', default='ok')
     
     archivo = filedialog.askopenfilename(filetypes=[("Archivos de Excel", "*.xlsx")])
     if archivo:
-        global nombres_institucional
         # Leer el archivo Excel institucional y extraer los nombres
         df = pd.read_excel(archivo)
         nombres_institucional = [normalizar_nombre(nombre) for nombre in df["Nombre"]]
-        # Comparar los nombres normalizados
-        nombres_no_encontrados = set(nombres_profesora) - set(nombres_institucional)
-        if nombres_no_encontrados:
-            # Mostrar un mensaje de ventana emergente con la opción de descargar el listado en PDF
-            mensaje_adicional = f"Total de estudiantes no encontrados: {len(nombres_no_encontrados)}\n\n{mensaje_recordatorio}"
-            ventana_no_encontrados(mensaje_adicional, list(nombres_no_encontrados))
+        
+        # Verificar si las columnas de calificaciones son las mismas en ambos archivos
+        if not verificar_columnas_calificaciones(df):
+            mensaje_adicional = f"Las columnas de calificaciones en su Excel no coinciden con el Excel Institucional.\n\n{mensaje_recordatorio}"
+            ventana_no_encontrados(mensaje_adicional, [])
+        else:
+            transferir_calificaciones(df)
 
+
+        
+
+def verificar_columnas_calificaciones(df):
+    # Verifica si las columnas de calificaciones en el Excel de la profesora coinciden con el Excel Institucional
+    columnas_profesora = set(columnas_calificaciones_profesora)
+    columnas_institucional = set(df.columns)
+    return columnas_profesora.issubset(columnas_institucional)
+
+def transferir_calificaciones(df):
+    # Transfiere las calificaciones del Excel de la profesora al Excel Institucional
+    # Esto asume que las columnas ya han sido verificadas
+    ubicacion_excel = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Archivos Excel", "*.xlsx")])
+    if ubicacion_excel:
+        # Copiar el DataFrame del Excel institucional para evitar cambios en el original
+        df_institucional = df.copy()
+        
+        # Agregar las calificaciones del Excel de la profesora al Excel Institucional
+        for columna in columnas_calificaciones_profesora:
+            df_institucional[columna] = df[columna]
+        
+        # Guardar el nuevo Excel
+        df_institucional.to_excel(ubicacion_excel, index=False)
+        mensaje_final.config(text="Calificaciones transferidas con éxito.")
+        messagebox.showinfo("Transferencia Completada", "Las calificaciones han sido transferidas exitosamente.\nEl archivo Excel con las calificaciones transferidas se ha guardado en:\n" + ubicacion_excel)
+        deshabilitar_botones()
+        
 
 def ventana_no_encontrados(mensaje, estudiantes):
 
@@ -81,7 +117,6 @@ def ventana_no_encontrados(mensaje, estudiantes):
     boton_pdf.pack()
 
 
-
 def generar_pdf_estudiantes_no_encontrados(estudiantes, ventana_emergente):
     ubicacion_pdf = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=[("Archivos PDF", "*.pdf")])
     if ubicacion_pdf:
@@ -102,7 +137,7 @@ def deshabilitar_botones():
     subir_institucional_button["state"] = "disabled"
     mensaje_final.config(text="Los nombres no coincidieron \npor favor verifique y cierre la aplicación.")
 
-# --------------------------------------- Interfaces de mi aplicacion -----------------------------------
+# --------------------------------------- Interfaces de mi aplicación -----------------------------------
 
 # Crear la ventana principal
 ventana = tk.Tk()
@@ -115,8 +150,6 @@ alto_pantalla = ventana.winfo_screenheight()
 # Calcula las coordenadas para centrar la ventana principal
 x = (ancho_pantalla - 400) // 2  # 400 es el ancho de tu ventana principal
 y = (alto_pantalla - 200) // 2   # 200 es la altura de tu ventana principal
-
-
 
 # Establece las dimensiones y la posición de la ventana principal
 ventana.geometry(f"400x200+{x}+{y}")
@@ -146,6 +179,3 @@ ventana.resizable(False, False)  # Ventana no redimensionable
 
 # Iniciar la aplicación
 ventana.mainloop()
-
-
-
