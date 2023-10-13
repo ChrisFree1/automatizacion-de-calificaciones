a5567afe750ef7f068556f06fe28420daaa37008
Christopher <chrisfree1122@gmail.com>
2023-10-12 22:30:07 -0500

    validaciones aprobadas para la tranferencia de notas

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, ttk, messagebox
 import pandas as pd
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
 
 # Variables para almacenar los nombres de estudiantes de ambos archivos
 nombres_profesora = []
@@ -12,46 +10,66 @@ nombres_institucional = []
 columnas_calificaciones_profesora = ["Primer Parcial", "Segundo Parcial", "Nota Final"]
 columnas_calificaciones_institucional = []
 
-def normalizar_nombre(nombre):
-    # Esta función normaliza el nombre eliminando espacios en blanco adicionales
-    # y convirtiéndolo a minúsculas
-    return nombre.strip().lower()
+# Definir df_institucional y df_profesora como variables globales
+df_institucional = pd.DataFrame()
+df_profesora = pd.DataFrame()
+archivos_cargados = False
+
+# ...
+
+def capitalizar_nombre(nombre):
+    # Divide el nombre en palabras
+    palabras = nombre.split()
+    # Capitaliza la primera letra de cada palabra
+    palabras_capitalizadas = [palabra.capitalize() for palabra in palabras]
+    # Une las palabras capitalizadas
+    return ' '.join(palabras_capitalizadas)
+
+# ...
+
 
 def cargar_archivo_profesora():
+    global nombres_profesora, columnas_calificaciones_profesora, df_profesora, archivos_cargados
     archivo = filedialog.askopenfilename(filetypes=[("Archivos de Excel", "*.xlsx")])
     if archivo:
-        global nombres_profesora, columnas_calificaciones_profesora
-        # Leer el archivo Excel de la profesora y extraer los nombres
-        df = pd.read_excel(archivo)
-        nombres_profesora = [normalizar_nombre(nombre) for nombre in df["Nombre"]]
+        # Leer el archivo Excel de la profesora y capitalizar los nombres
+        df_profesora = pd.read_excel(archivo)
+        df_profesora['Nombre'] = df_profesora['Nombre'].apply(capitalizar_nombre)
+        nombres_profesora = [nombre.lower() for nombre in df_profesora["Nombre"]]
         # Al cargar el archivo de la profesora, habilitar el botón "Subir Excel Institucional"
-        columnas_calificaciones_profesora = df.columns
+        columnas_calificaciones_profesora = df_profesora.columns
         subir_institucional_button["state"] = "normal"
-        
+        archivos_cargados = True
+
 def cargar_archivo_institucional():
+    global nombres_institucional, columnas_calificaciones_institucional, df_institucional, archivos_cargados
+    if not archivos_cargados:
+        messagebox.showerror("Error", "Cargue primero el archivo de la profesora.")
+        return
     mensaje_advertencia = "En el Excel que subió anteriormente, los nombres de sus estudiantes deben estar en el Excel institucional para poder transferir las calificaciones. \nCaso contrario no se procederá a transferir."
     mensaje_adicional = ""
     mensaje_recordatorio = "¡Recuerde! Los nombres de sus estudiantes deben ser los mismos que están en el Excel Institucional."
-    
+
     messagebox.showinfo("Dato Importante", mensaje_advertencia, icon='info', default='ok')
-    
+
     archivo = filedialog.askopenfilename(filetypes=[("Archivos de Excel", "*.xlsx")])
     if archivo:
         global nombres_institucional, columnas_calificaciones_institucional
-        # Leer el archivo Excel institucional y extraer los nombres
+        # Leer el archivo Excel institucional y capitalizar los nombres
         df = pd.read_excel(archivo)
-        nombres_institucional = [normalizar_nombre(nombre) for nombre in df["Nombre"]]
+        df['Nombre'] = df['Nombre'].apply(capitalizar_nombre)
+        df['Nombre'] = df['Nombre'].apply(lambda nombre: nombre.lower())  # Asegurar que la columna 'Nombre' esté normalizada
+        nombres_institucional = df['Nombre'].tolist()
+        df_institucional = df  # Establecer df_institucional como el DataFrame leído
         # Al cargar el archivo institucional, habilitar el botón "Transferir Calificaciones" y verificar las columnas
         columnas_calificaciones_institucional = df.columns
         if not list(columnas_calificaciones_profesora) == list(columnas_calificaciones_institucional):
             mensaje_adicional = "Las siguientes columnas de calificaciones no coinciden con el Excel institucional:\n"
             columnas_no_coincidentes = set(columnas_calificaciones_profesora).difference(columnas_calificaciones_institucional)
             mensaje_adicional += f"- {', '.join(columnas_no_coincidentes)}\nPor favor, verifique los nombres de las columnas."
-            ventana_no_encontrados(mensaje_adicional, [])
-
+            ventana_no_encontrados(mensaje_adicional, columnas_no_coincidentes)
         else:
             transferir_calificaciones_button["state"] = "normal"
-
 
 def ventana_no_encontrados(mensaje, columnas_no_coincidentes):
     def on_cierre():
@@ -84,9 +102,9 @@ def ventana_no_encontrados(mensaje, columnas_no_coincidentes):
     label = ttk.Label(ventana_emergente, text=mensaje, foreground="black", wraplength=380)
     label.pack(pady=20)
 
-# Resto del código sin cambios
-
 def transferir_calificaciones():
+    global nombres_profesora, nombres_institucional, df_institucional, df_profesora
+
     if not nombres_profesora or not nombres_institucional:
         messagebox.showerror("Error", "Primero cargue los archivos de la profesora y el institucional.")
         return
@@ -97,20 +115,34 @@ def transferir_calificaciones():
         mensaje_adicional += f"- {', '.join(columnas_no_coincidentes)}\nPor favor, verifique los nombres de las columnas."
         ventana_no_encontrados(mensaje_adicional, columnas_no_coincidentes)
     else:
-        # Normalizar los nombres para asegurarse de que coincidan
-        nombres_profesora = [normalizar_nombre(nombre) for nombre in nombres_profesora]
-        nombres_institucional = [normalizar_nombre(nombre) for nombre in nombres_institucional]
+        # Capitalizar los nombres en ambas listas
+        df_profesora['Nombre'] = df_profesora['Nombre'].apply(capitalizar_nombre)
+        nombres_profesora = [nombre.lower() for nombre in df_profesora['Nombre']]
 
-        # Crear un diccionario para mapear los nombres de la profesora a los nombres institucionales
-        mapeo_nombres = dict(zip(nombres_profesora, nombres_institucional))
+        # Capitalizar los nombres en df_institucional
+        df_institucional['Nombre'] = df_institucional['Nombre'].apply(capitalizar_nombre)
 
-        # Crear un DataFrame para las calificaciones
-        df_profesora = pd.read_excel(archivo_profesora)
-        df_institucional = pd.read_excel(archivo_institucional)
+        # Agregar impresiones de diagnóstico
+        print("Nombres capitalizados en df_profesora:")
+        print(df_profesora.head())
 
-        # Actualizar las notas en el DataFrame institucional
+        # Crear un diccionario para mapear los nombres capitalizados de la profesora a los nombres capitalizados de df_institucional
+        mapeo_nombres = dict(zip(df_profesora['Nombre'], df_institucional['Nombre']))
+
+        # Agregar impresiones de diagnóstico
+        print("Mapeo de nombres:")
+        print(mapeo_nombres)
+
+        # Transferir calificaciones
         for columna in columnas_calificaciones_profesora:
-            df_institucional[columna] = df_profesora["Nombre"].map(mapeo_nombres).map(df_profesora[columna])
+            for index, row in df_profesora.iterrows():
+                nombre_estudiante = row['Nombre']
+                calificacion = row[columna]
+                df_institucional.loc[df_institucional['Nombre'] == mapeo_nombres[nombre_estudiante], columna] = calificacion
+
+        # Agregar impresiones de diagnóstico
+        print("Después de actualizar df_institucional:")
+        print(df_institucional.head())
 
         # Guardar el archivo actualizado
         ubicacion_nuevo_excel = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Archivos Excel", "*.xlsx")])
@@ -121,6 +153,9 @@ def transferir_calificaciones():
             deshabilitar_botones()
 
 
+
+
+
 # Función para deshabilitar los botones y mostrar el mensaje
 def deshabilitar_botones():
     subir_profesora_button["state"] = "disabled"
@@ -128,7 +163,7 @@ def deshabilitar_botones():
     transferir_calificaciones_button["state"] = "disabled"
     mensaje_final.config(text="La transferencia de calificaciones ha sido completada.\nPor favor, verifique y cierre la aplicación.")
 
-# --------------------------------------- Interfaces de mi aplicacion -----------------------------------
+# --------------------------------------- Interfaces de mi aplicación -----------------------------------
 
 # Crear la ventana principal
 ventana = tk.Tk()
